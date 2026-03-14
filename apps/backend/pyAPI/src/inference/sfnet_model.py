17b4a1dd6a03606f27032b201db270f99f3c30f7
FNMF <1733429203@qq.com>
2026-03-14 18:13:43 +0800

    修复了python环境问题

---
+++ apps/backend/pyAPI/src/inference/sfnet_model.py
@@ -0,0 +1,20 @@
+import torch
+
+class SFNetModel:
+
+    def __init__(self):
+
+        print("Loading SFNet model...")
+
+        # 示例
+        # self.model = torch.load("sfnet.pth")
+
+    def predict(self, image):
+
+        # TODO: 模型推理
+        confidence = 0.8
+
+        return confidence
+
+
+sfnet_model = SFNetModel()
